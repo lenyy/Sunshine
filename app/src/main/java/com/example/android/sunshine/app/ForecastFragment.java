9bbda177dd51705611b6f09303db421052589716
lenyy <pedro.rosa90@gmail.com>
2015-11-02 20:55:57 +0000

    some stuff

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -20,9 +20,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * Created by pedro on 02-11-2015.
- */
+
+
 public class ForecastFragment extends Fragment {
 
     ArrayAdapter<String> adapter;
@@ -80,11 +79,14 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
+                urlConnection.setDoInput(true);
                 urlConnection.connect();
 
                 // Read the input stream intkOnMainThreadExceptiono a String
